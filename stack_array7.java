5b24459398b75b2d7a5ed2480b17b2fc138c1d16
Sukanya570 <Sukanyarpatil25@gmail.com>
2026-01-01 19:20:51 -0800

    Create stack_array7.java

---
+++ stack_array7.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class Solution {
+
+    // Function to return precedence of operators
+    private static int precedence(char ch) {
+        switch (ch) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
+
+
+    public static String infixToPostfix(String expression) {
+        StringBuilder result = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+
+            
+            if (Character.isLetterOrDigit(ch)) {
+                result.append(ch);
+            }
+        
+            else if (ch == '(') {
+                stack.push(ch);
+            }
+            
+            else if (ch == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    result.append(stack.pop());
+                }
+                stack.pop();
+            }
+        
+            else {
+                while (!stack.isEmpty() && precedence(ch) <= precedence(stack.peek())) {
+                    result.append(stack.pop());
+                }
+                stack.push(ch);
+            }
+        }
+
+        
+        while (!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(infixToPostfix(str));
+        sc.close();
+    }
+}
